2977f4f00d77c81f00c5d605c5866d0bb5eaea8f
KhairuzzamanCP <mdkhariuzzaman@gmail.com>
2024-03-07 20:49:17 +0600

    Validation in Django REST Framework

---
+++ Validation in Django REST Framework/S5/api/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework import serializers
+from .models import Student
+
+# Validators
+def start_with_k(value):
+    if value[0].lower() != 'k':
+        raise serializers.ValidationError('Name Should be start with K')
+
+class StudentSeralizer(serializers.Serializer):
+    name = serializers.CharField(max_length = 100, validators =[start_with_k])
+    roll = serializers.IntegerField()
+    city = serializers.CharField(max_length = 100)
+
+
+    '''
+    create হলো এই মেথডের নাম, এটি একটি নতুন মডেল ইনস্ট্যান্স তৈরি করতে ব্যবহৃত হয়।
+    self এটি একটি ক্লাস মেথড, এটি ক্লাস নিজেই মেথডটি ব্যবহার করতে পারে।
+    validated_data এটি একটি ডিকশনারি, এটি একটি ইনকামিং ডেটা হিসেবে প্রদান হয় এবং এই ডেটা একটি ভ্যালিডেটেড সিরিয়ালাইজড ডাটা।
+    Student.objects.create(**validated_data) এটি একটি মডেল ম্যানেজার মেথড, এটি একটি নতুন মডেল ইনস্ট্যান্স তৈরি করে এবং সেই ইনস্ট্যান্সে ডাটা সেট করে যা validated_data থেকে আসে।
+    **validated_data এটি একটি ডাটা কর্মফল হলে যা সিরিয়ালাইজ হয়ে থাকে, সেই ডাটা কেউ মডেল ইনস্ট্যান্সে সেট করতে একটি উপায়।
+    এই কোডে, একটি নতুন মডেল ইনস্ট্যান্স তৈরি করা হয় Student মডেলের সাথে এবং সেই ইনস্ট্যান্সে validated_data থেকে আসা ডাটা সেট করা হয়।
+    '''
+    def create(self, validated_data):
+        return Student.objects.create(**validated_data)
+    '''
+    create: এটি মেথডের নাম, এই মেথডের মাধ্যমে নতুন ইনস্ট্যান্স তৈরি হয়।
+
+    self: এটি একটি ক্লাস মেথড, এটি ক্লাস নিজেই মেথডটি ব্যবহার করতে পারে।
+
+    validated_data: এটি একটি ডিকশনারি, এটি সিরিয়ালাইজার দ্বারা ভ্যালিডেট এবং ডিসিরিয়ালাইজ করা হয় ডাটা ধারণ করে।
+
+    Student.objects.create(**validated_data): এটি একটি মডেল ম্যানেজার মেথড, এটি একটি নতুন মডেল ইনস্ট্যান্স তৈরি করে এবং সেই ইনস্ট্যান্সে ডাটা সেট করে যা validated_data থেকে আসে।
+
+    **validated_data: এটি ডাটা কর্মফল হলে, সেই ডাটা কে মডেল ইনস্ট্যান্সে সেট করতে একটি উপায়, ডাটা একেবারে সিরিয়ালাইজ হয়ে থাকে এবং মডেল ফিল্ডের সাথে যোগাযোগ করতে এটি অব্যহৃত হয়।
+
+    সারাংশঃ এই কোডটি ভ্যালিডেটেড ডাটা নেয় এবং একটি নতুন Student মডেল ইনস্ট্যান্স তৈরি করে যা সেই ডাটা দিয়ে সেট করে।
+    
+    '''
+    def update(self, instance, validated_data):
+        '''
+        এই লাইন দ্বারা বুঝাচ্ছে আপডেট হওয়ার আগের ডাটা
+        '''
+        print(instance.name)
+        instance.name = validated_data.get('name', instance.name)
+        '''
+        এই লাইন দ্বারা বুঝাচ্ছে আপডেট হওয়ার পর ডাটা
+        '''
+        print(instance.name)
+        instance.roll = validated_data.get('roll', instance.roll)
+        print(instance.roll)
+        instance.city = validated_data.get('city', instance.city)
+        print(instance.city)
+        instance.save()
+        return instance
+    # Field Level Validation
+    def validate_roll(self,value):
+        if value >= 200:
+            raise serializers.ValidationError("Seat Full")
+        return value
+
+    # Object Level Validation
+    def validate(self, data):
+        nm = data.get('name')
+        ct = data.get('city')
+        if nm.lower() == 'rakib' and ct.lower() != 'Rangpur':
+            raise serializers.ValidationError('city must be Rangpur ')
+        return data
+   
